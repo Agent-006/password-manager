637270f35b5852e4b694af765f9aed538874ee4f
Sagar~ <sagargope2001@gmail.com>
2024-03-22 23:51:57 +0530

    style: bug fixes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,8 @@ import PassCards from "../PassCards/PassCards";
 
 function Hero() {
   return (
-    <div className="w-full h-full px-10 py-5 flex flex-col justify-center items-center gap-5">
-      <div className="flex flex-col justify-center items-center gap-5 tracking-tighter">
+    <div className="w-full h-full px-10 py-5 flex flex-col justify-center items-center gap-1">
+      <div className="w-full h-1/2 flex flex-col justify-center items-center gap-5 tracking-tighter py-2">
         <h1 className="text-5xl font-bold text-gray-200">Keep your password</h1>
 
         <h1 className="text-6xl font-bold bg-gradient-to-r from-rose-400 from-10% via-rose-600 via-40% to-purple-600 bg-clip-text text-transparent">
@@ -16,13 +16,45 @@ function Hero() {
           </div>
         </button>
       </div>
-      <div className="w-full h-96 gap-2 flex flex-col">
-        <PassCards
-          website={"www.google.com"}
-          username={"Sagar01010"}
-          email={"sagar@mail.com"}
-          password={"******"}
-        />
+      <div className="w-full h-1/2 overflow-hidden">
+        <div className="w-full h-full gap-2 px-2 py-5 flex flex-col justify-start items-center overflow-y-scroll">
+          <PassCards
+            website={"www.google.com"}
+            username={"Sagar01010"}
+            email={"sagar@mail.com"}
+            password={"******"}
+          />
+          <PassCards
+            website={"www.google.com"}
+            username={"Sagar01010"}
+            email={"sagar@mail.com"}
+            password={"******"}
+          />
+          <PassCards
+            website={"www.google.com"}
+            username={"Sagar01010"}
+            email={"sagar@mail.com"}
+            password={"******"}
+          />
+          <PassCards
+            website={"www.google.com"}
+            username={"Sagar01010"}
+            email={"sagar@mail.com"}
+            password={"******"}
+          />
+          <PassCards
+            website={"www.google.com"}
+            username={"Sagar01010"}
+            email={"sagar@mail.com"}
+            password={"******"}
+          />
+          <PassCards
+            website={"www.google.com"}
+            username={"Sagar01010"}
+            email={"sagar@mail.com"}
+            password={"******"}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/PassCards/PassCards.jsx
+++ src/components/PassCards/PassCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PassCards({ website, username = "", email = "", password }) {
   return (
-    <div className="bg-gradient-to-r from-rose-400 from-10% via-rose-600 via-40% to-purple-600 rounded-md overflow-hidden p-[1px] font-semibold text-xs">
+    <div className="w-full h-full bg-gradient-to-r from-rose-400 from-10% via-rose-600 via-40% to-purple-600 rounded-md p-[1px] font-semibold text-xs">
       <div className="w-full py-3 px-5 rounded-md flex justify-between text-zinc-200 bg-zinc-950/90 backdrop-blur-md">
         <div className="px-2">{website}</div>
         <div className="px-2">{username}</div>
